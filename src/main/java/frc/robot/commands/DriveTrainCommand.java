fb4d0dfcab4845d202bdaaf584bd4c3f813e1c01
Jobot_pog <jb1dsi275@gmail.com>
2022-02-09 17:54:11 -0500

    Drive Turret & Shooter Motors

--- src/main/java/frc/robot/commands/DriveTrainCommand.java
+++ src/main/java/frc/robot/commands/DriveTrainCommand.java
@@ -6,20 +6,21 @@ package frc.robot.commands;
 
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
-import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.DriveTrainSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
-public class ShooterCommand extends CommandBase {
+public class DriveTrainCommand extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ShooterSubsystem m_subsystem;
+  private final DriveTrainSubsystem m_subsystem;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public ShooterCommand(ShooterSubsystem subsystem) {
+  public DriveTrainCommand(DriveTrainSubsystem subsystem) {
     m_subsystem = subsystem;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
@@ -33,7 +34,8 @@ public class ShooterCommand extends CommandBase {
   @Override
   public void execute()
   {
-    ShooterSubsystem.joystickShoot(RobotContainer.driveStick); //switch .joystickShootPID to .joystickShoot for voltage
+    DriveTrainSubsystem.xboxControllerDrive(RobotContainer.driveXboxController, .4);
+    //DriveTrainSubsystem.joystickDrive(RobotContainer.driveStick, .25);
   }
 
   // Called once the command ends or is interrupted.
